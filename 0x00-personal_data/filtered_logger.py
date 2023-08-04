8f977da7cddab8085042758682fbf6e839be5445
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-04 03:15:39 +0100

    task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,9 @@ def filter_datum(fields, redaction, message, separator):
     returns the log message obfuscated
     """
     for field in fields:
+        """ {field}=.*?; means from {field}= to ;
+        .*? is non-gready copy. it copys from = and ends at ;
+        """
         message = re.sub(rf'{field}=.*?;',
                          f'{field}={redaction}{separator}', message)
     return message
