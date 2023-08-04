8166fdd517c5e386b89192a8dadb1709f7eb0406
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-04 03:15:35 +0100

    task

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Regex-ing
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    returns the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(rf'{field}=.*?;',
+                         f'{field}={redaction}{separator}', message)
+    return message
