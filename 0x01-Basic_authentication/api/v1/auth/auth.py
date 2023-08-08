2ce9b4294c214b1af10157031cefca589fa9caa8
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-08 02:49:37 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,7 @@ class Auth:
         """Require authentication"""
         if path:
             return False
-    
+
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
         if request:
@@ -22,4 +22,4 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Current User"""
         if request is None:
-            return None 
+            return None
