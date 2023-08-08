505f6b7d8ad9085849d4281be8cee66831d24d86
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-08 23:56:36 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,3 +23,25 @@ class Auth:
         """Current User"""
         if request is None:
             return None
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Defines which routes don't need authentication"""
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith("/"):
+            path = path + "/"
+        
+        new_excluded_paths = []
+        for excluded_path in excluded_paths:
+            if not excluded_path.endswith("/"):
+                excluded_path = excluded_path + "/"
+            new_excluded_paths.append(excluded_path)
+        excluded_paths = new_excluded_paths
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
