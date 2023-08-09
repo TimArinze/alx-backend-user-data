72b1e5346e46b26b84ba1d61d97e201f35dbbb71
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-09 02:42:14 +0100

    task

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -52,7 +52,7 @@ def before_request() -> str:
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
-            abort(403)    
+            abort(403)
 
 
 if __name__ == "__main__":
