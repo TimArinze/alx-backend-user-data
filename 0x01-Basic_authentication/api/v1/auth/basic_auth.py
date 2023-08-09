c647cd749c759456be8c670fbe3e2d17cbcf6ad9
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-09 03:06:49 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Auth"""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Base64 part"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header.startswith("Basic ") is False:
+            return None
+        else:
+            return authorization_header.split(" ")[-1]
