09e758ce00e656b5026ca57d7ebe48db424cd087
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-09 01:21:48 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,11 +9,6 @@ from typing import List, TypeVar
 class Auth:
     """Authentication class"""
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require authentication"""
-        if path:
-            return False
-
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
         if request:
