cf5be5ebf42fb064234d30604ed522d531df8cb0
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-09 03:26:27 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Basic Auth
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -19,3 +21,18 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split(" ")[-1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str)\
+            -> str:
+        """Base64 decode"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode("utf-8")
+            return decoded_string
+        except binascii.Error:
+            return None
