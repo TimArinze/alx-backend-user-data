225580db9da5e5627f3d5dc3c82f648982465f0b
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-11 01:17:34 +0100

    task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,7 +11,10 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        if user_id is None or type(user_id) != str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None:
+            return None
+        if type(user_id) != str:
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
