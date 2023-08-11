a6838c108bbc97c5bce55d19ff4213f8a7443e41
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-11 02:19:00 +0100

    task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,4 +32,4 @@ class SessionAuth(Auth):
         """returns a user instance based on a cookie value"""
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
-        return User.get(user_id)        
+        return User.get(user_id)
