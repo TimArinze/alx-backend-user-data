3e969d1d89786a4754cb4b49be3eb2e55074807c
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-11 02:17:44 +0100

    task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@ Session Auth
 """
 from api.v1.auth.auth import Auth
 import uuid
+from typing import TypeVar
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -25,3 +27,9 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """returns a user instance based on a cookie value"""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)        
