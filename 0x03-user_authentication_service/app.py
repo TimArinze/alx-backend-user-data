badfb25fea57759bdb543775af459942af8c095d
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-16 00:15:43 +0100

    task

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic Flask App"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def basic():
+    """Basic flask app"""
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
