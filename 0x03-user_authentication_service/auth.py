023efa96967813ff229a95b3a98d9e6ed9b07d02
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-15 23:22:40 +0100

    task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,6 +13,9 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), salt)
 
 
+"""Register user"""
+
+
 class Auth:
     """Auth class to interact with the authentication database"""
 
@@ -23,7 +26,8 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Register User"""
         try:
-            self._db.find_user_by(email=email)
-            raise ValueError("User {} already exists".format(email))
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return Auth._db.add_user(email, _hash_password(password))
