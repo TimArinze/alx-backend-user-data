21f1e6d24655ace1997e51a8f322400fe10800ce
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-15 19:34:18 +0100

    task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,3 +8,23 @@ def _hash_password(password: str) -> bytes:
     """Hash password"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database"""
+
+    def __init__(self):
+        """Initializing"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register User"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
