e0f6cb98077e9780130a45116dedc82c3e598e9f
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-16 23:52:03 +0100

    task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,7 +52,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
-            new_session_id = setattr(user, "session_id", session_id)
+            setattr(user, "session_id", session_id)
             self._db._session.commit()
             return user.session_id
         except NoResultFound:
@@ -81,5 +81,14 @@ class Auth:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return user.reset_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str):
+        """Update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
         except NoResultFound:
             raise ValueError
