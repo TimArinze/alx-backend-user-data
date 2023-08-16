7815a377226cd2de37d2ae79d5cf050773afe4a7
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-16 01:42:48 +0100

    task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -26,6 +26,19 @@ def register_user():
             return jsonify({"email": email, "message": "user created"}), 200
         except ValueError:
             return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Log in"""
+    data = request.form
+    email = data["email"]
+    password = data["password"]
+    if AUTH.valid_login(email, password):
+        AUTH.create_session(email)
+        return jsonify({"email": email, "message": "logged in"}), 200
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
