e652e614316ac4e9c0716e9ef62129094904c785
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-16 16:59:48 +0100

    task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,7 +49,7 @@ def logout():
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        AUTH.destroy_session(int(user.id))
+        AUTH.destroy_session(user.id)
         return redirect("/")
     else:
         abort(403)
