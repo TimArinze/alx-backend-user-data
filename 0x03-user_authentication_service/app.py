c1492dfbf248769434dfcb2fe3460b5626cfb808
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-16 23:35:32 +0100

    task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,5 +66,16 @@ def user_profile():
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def reset_password():
+    """Get reset password token"""
+    email = request.form["email"]
+    reset_token = AUTH.get_reset_password_token(email)
+    if reset_token:
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
